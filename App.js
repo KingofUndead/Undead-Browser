5a02283223fc8c5b74be538b34854f9bd9473ec3
King_of_Undead <victorvsgaming@gmail.com>
2021-12-18 23:49:31 -0600

    hello won't render

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './style.scss';
+
+export default function App(props) {
+    <>
+    <p>Hello {this.props.user}</p>
+    </>
+}
+
+ReactDOM.render(<App user="Dev"/>, document.getElementById('root'));
+//<div class="bgimg></div>"
+//<input type="text" className="searchbar"></input>
--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+//const { generateKey } = require('crypto')
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
+
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 800,
     webPreferences: {
+      nodeIntergration: false,
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
       preload: path.join(__dirname, 'preload.js')
     }
   })
-
+  win.webContents.openDevTools()
   win.loadFile('index.html')
 }
 
